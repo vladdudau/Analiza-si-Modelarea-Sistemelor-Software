017895953ae98112ef820fa8d0edd486bbdbba27
vladdudau <dudau.vlad@gmail.com>
2023-02-04 17:01:58 +0200

    First Commit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+﻿namespace ProiectAnaliza.Migrations
+{
+    using ProiectAnaliza.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProiectAnaliza.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "ProiectAnaliza.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ProiectAnaliza.Models.ApplicationDbContext context)
+        {
+            var admin = new List<Administration>
+            {
+                 new Administration { Name = "Durata programarii in minute [Default:90]",   Value = "90"},
+                 new Administration { Name = "Programul de lucru incepe la ora: [Default:8]",   Value = "8"},
+                 new Administration { Name = "Programul de lucru se termina la ora [Default:17]",   Value = "17"},
+            };
+
+            admin.ForEach(s => context.Administrations.AddOrUpdate(p => p.Name, s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Hangfire;
+using ProiectAnaliza.Controllers;
+using ProiectAnaliza.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProiectAnaliza.Startup))]
+namespace ProiectAnaliza
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration
+               .UseSqlServerStorage("DefaultConnection");
+            ConfigureAuth(app);
+            CreateAdminUserAndApplicationRoles();
+
+            app.UseHangfireDashboard();
+            BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget!"));
+            AppointmentController obj = new AppointmentController();
+
+
+
+            RecurringJob.AddOrUpdate(() => obj.SendEmailAppointmentNotification(), "0 11 * * *");
+
+
+
+
+
+
+            app.UseHangfireServer();
+        }
+        private void CreateAdminUserAndApplicationRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new
+            RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new
+            UserStore<ApplicationUser>(context));
+            // Se adauga rolurile aplicatiei
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+                var user = new ApplicationUser();
+                user.UserName = "neverloseyourhope04@gmail.com";
+                user.Email = "neverloseyourhope04@gmail.com";
+                var adminCreated = UserManager.Create(user, "****");
+                if (adminCreated.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+            if (!roleManager.RoleExists("Editor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Editor";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
